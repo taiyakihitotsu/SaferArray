fb397212af75cd86c022b9576158bf577a84c156
taiyaki <taiyakihitotsu@proton.me>
2024-01-30 05:22:49 +0100

    [fix] Vmul, removing with TLesserUnion part, which causes Type Level Infinite Loop it cannot be detected in type level (maybe).

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,11 +4,11 @@ import {
     ,Vzero,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10
     ,TpseudoNumber,TELesserUnion,TLesserUnion
     ,RtoV,VtoR
-    ,Vadd,Vmin
+    ,Vadd,Vmin,Vmul
     ,TWrapArray
     ,WrapArray,UnwrapArray
     ,inWrapRest,inWrapConj,inWrapGet
-    ,inWrapTake,inWrapDrop,inWrapConcat,inWrapFilter} from "../src/index"
+    ,inWrapTake,inWrapDrop,inWrapConcat,inWrapFilter,inWrapRemove} from "../src/index"
 
 test('RtoV', ()=>{
     expect(RtoV(0)).toEqual(Vzero)
@@ -34,4 +34,54 @@ test('Vmin', ()=>{
     expect(Vmin(V1,Vzero)).toEqual(V1)
     // Vmin(Vzero, V1) // type error.
     expect(Vmin(Vmin(v30,V10),V10)).toEqual(V10)
+})
+
+test('Vmul', ()=>{
+    const v30 = [[[[[[[[[[[[[[[[[[[[V10]]]]]]]]]]]]]]]]]]]]
+    expect(Vmul(Vzero,Vzero)).toEqual(Vzero)
+    expect(Vmul(V1,Vzero)).toEqual(Vzero)
+    expect(Vmul(Vzero,V1)).toEqual(Vzero)
+    expect(Vmul(v30,Vzero)).toEqual(Vzero)
+    expect(Vmul(Vzero,v30)).toEqual(Vzero)
+    expect(Vmul(V1,V1)).toEqual(V1)
+    expect(Vmul(V1,v30)).toEqual(v30)
+    expect(Vmul(v30,V1)).toEqual(v30)
+    // expect(Vmul(V1,v30)).toEqual(V1)
+    // expect(Vmul(V1,v30)).toEqual(V1)
+    expect(Vmul(V2,V3)).toEqual(V6)
+    expect(Vmul(V6,V5)).toEqual(v30)
+    expect(Vmul(V5,V6)).toEqual(v30)
+})
+
+test('WrapArray', ()=>{
+})
+
+test('UnwrapArray', ()=>{
+})
+
+test('inWrapRest', ()=>{
+})
+
+test('inWrapConj', ()=>{
+})
+
+test('inWrapRest', ()=>{
+})
+
+test('inWrapGet', ()=>{
+})
+
+test('inWrapConcat', ()=>{
+})
+
+test('inWrapTake', ()=>{
+})
+
+test('inWrapDrop', ()=>{
+})
+
+test('inWrapFilter', ()=>{
+})
+
+test('inWrapRemove', ()=>{
 })
